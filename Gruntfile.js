8764caf966f897a427ee91d34a20cab6738562f5
Karun Japhet <karun@japhet.in>
2015-04-09 00:50:56 +0530

    Migrating to a cleaner config

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,136 +11,195 @@ module.exports = function (grunt) {
     require('load-grunt-tasks')(grunt);
     require('time-grunt')(grunt);
 
+    var appConfig = {
+        app: 'web',
+        dist: 'dist'
+    };
+
+    var deployConfig = grunt.file.readJSON('.deploy-config.json');
+
     grunt.initConfig({
-        appConfig: {
-            app: 'web',
-            dist: 'dist'
-        },
-        watch: {
+            appConfig: appConfig,
+            watch: {
+                coffee: {
+                    files: ['<%= appConfig.app %>/coffee/{,*/}*.coffee'],
+                    tasks: ['coffee']
+                },
+                less: {
+                    files: ['<%= appConfig.app %>/less/{,*/}*.less'],
+                    tasks: ['less']
+                },
+                gruntfile: {
+                    files: ['Gruntfile.js']
+                },
+                livereload: {
+                    options: {
+                        livereload: '<%= connect.options.livereload %>'
+                    },
+                    files: [
+                        '<%= appConfig.app %>/*.html',
+                        '{.tmp,<%= appConfig.app %>}/css/{,*/}*.css',
+                        '{.tmp,<%= appConfig.app %>}/js/{,*/}*.js',
+                        '<%= appConfig.app %>/img/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+                    ]
+                }
+            },
+            connect: {
+                options: {
+                    port: 9000,
+                    // change this to '0.0.0.0' to access the server from outside
+                    hostname: 'localhost',
+                    livereload: 35729
+                },
+                livereload: {
+                    options: {
+                        open: true,
+                        base: [
+                            '.tmp',
+                            '<%= appConfig.app %>'
+                        ]
+                    }
+                },
+                test: {
+                    options: {
+                        port: 9001,
+                        base: [
+                            '.tmp',
+                            'test',
+                            '<%= appConfig.app %>'
+                        ]
+                    }
+                },
+                dist: {
+                    options: {
+                        base: '<%= appConfig.dist %>'
+                    }
+                }
+            },
+            clean: {
+                dist: {
+                    files: [{
+                        dot: true,
+                        src: [
+                            '.tmp',
+                            '<%= appConfig.dist %>/*',
+                            '!<%= appConfig.dist %>/.git*'
+                        ]
+                    }]
+                },
+                server: '.tmp'
+            },
             coffee: {
-                files: ['<%= appConfig.app %>/coffee/{,*/}*.coffee'],
-                tasks: ['coffee']
+                dist: {
+                    files: [{
+                        expand: true,
+                        cwd: '<%= appConfig.app %>/coffee',
+                        src: '{,*/}*.coffee',
+                        dest: '<%= appConfig.app %>/js',
+                        ext: '.js'
+                    }]
+                }
             },
             less: {
-                files: ['<%= appConfig.app %>/less/{,*/}*.less'],
-                tasks: ['less']
+                dist: {
+                    files: {
+                        '<%= appConfig.app %>/css/main.css': ['<%= appConfig.app %>/less/main.less']
+                    },
+                    options: {
+                        sourceMap: false,
+                        sourceMapFilename: '<%= appConfig.app %>/less/main.css.map',
+                        sourceMapBasepath: '<%= appConfig.app %>/',
+                        sourceMapRootpath: '/'
+                    }
+                }
             },
-            gruntfile: {
-                files: ['Gruntfile.js']
-            },
-            livereload: {
-                options: {
-                    livereload: '<%= connect.options.livereload %>'
+            copy: {
+                dist: {
+                    files: [{
+                        expand: true,
+                        dot: true,
+                        cwd: '<%= appConfig.app %>',
+                        dest: '<%= appConfig.dist %>',
+                        src: [
+                            '*.{ico,png,txt}',
+                            'fonts/{,*/}*.*',
+                            '.htaccess',
+                            'images/{,*/}*.{webp,gif}'
+                        ]
+                    }]
                 },
-                files: [
-                    '<%= appConfig.app %>/*.html',
-                    '{.tmp,<%= appConfig.app %>}/css/{,*/}*.css',
-                    '{.tmp,<%= appConfig.app %>}/js/{,*/}*.js',
-                    '<%= appConfig.app %>/img/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
-                ]
-            }
-        },
-        connect: {
-            options: {
-                port: 9000,
-                // change this to '0.0.0.0' to access the server from outside
-                hostname: 'localhost',
-                livereload: 35729
+                server: {
+                    files: [{
+                        expand: true,
+                        dot: true,
+                        cwd: '<%= appConfig.app %>/bower_components/font-awesome/fonts/',
+                        dest: '<%= appConfig.app %>/fonts/font-awesome',
+                        src: ['*']
+                    }, {
+                        expand: true,
+                        dot: true,
+                        cwd: '<%= appConfig.app %>/bower_components/bootstrap/dist/fonts/',
+                        dest: '<%= appConfig.app %>/fonts/glyphicons',
+                        src: ['*']
+                    }]
+                }
             },
-            livereload: {
-                options: {
-                    open: true,
-                    base: [
-                        '.tmp',
-                        '<%= appConfig.app %>'
+            cssmin: {
+                dist: {
+                    files: [{
+                        expand: true,
+                        cwd: '<%= appConfig.app %>/css',
+                        src: ['*.css', '!*.min.css'],
+                        dest: '<%= appConfig.dist %>/css',
+                        ext: '.min.css'
+                    }]
+                }
+            },
+            html_minify: {
+                options: {},
+                //files: {
+                //    'dest/index.html': ['src/index.html']
+                //}
+                all: {
+                    files: [
+                        {
+                            expand: true,
+                            cwd: '<%= appConfig.app %>',
+                            src: ['*.html'],
+                            dest: '<%= appConfig.dist %>',
+                            ext: '.html'
+                        }
                     ]
                 }
             },
-            test: {
-                options: {
-                    port: 9001,
-                    base: [
-                        '.tmp',
-                        'test',
-                        '<%= appConfig.app %>'
-                    ]
-                }
-            },
-            dist: {
-                options: {
-                    base: '<%= appConfig.dist %>'
+            'sftp-deploy': {
+                build: {
+                    auth: deployConfig.auth,
+                    cache: '.sftpCache.json',
+                    src: appConfig.dist,
+                    dest: deployConfig.dest,
+                    exclusions: ['dist/**/.DS_Store', 'dist/**/Thumbs.db', 'dist/tmp'],
+                    serverSep: '/',
+                    concurrency: 4,
+                    progress: true
                 }
             }
-        },
-        clean: {
-            dist: {
-                files: [{
-                    dot: true,
-                    src: [
-                        '.tmp',
-                        '<%= appConfig.dist %>/*',
-                        '!<%= appConfig.dist %>/.git*'
-                    ]
-                }]
-            },
-            server: '.tmp'
-        },
-        coffee: {
-            dist: {
-                files: [{
-                    expand: true,
-                    cwd: '<%= appConfig.app %>/coffee',
-                    src: '{,*/}*.coffee',
-                    dest: '<%= appConfig.app %>/js',
-                    ext: '.js'
-                }]
-            }
-        },
-        less: {
-            dist: {
-                files: {
-                    '<%= appConfig.app %>/css/main.css': ['<%= appConfig.app %>/less/main.less']
-                },
-                options: {
-                    sourceMap: false,
-                    sourceMapFilename: '<%= appConfig.app %>/less/main.css.map',
-                    sourceMapBasepath: '<%= appConfig.app %>/',
-                    sourceMapRootpath: '/'
-                }
-            }
-        },
-        copy: {
-            dist: {
-                files: [{
-                    expand: true,
-                    dot: true,
-                    cwd: '<%= appConfig.app %>',
-                    dest: '<%= appConfig.dist %>',
-                    src: [
-                        '*.{ico,png,txt}',
-                        'fonts/{,*/}*.*',
-                        '.htaccess',
-                        'images/{,*/}*.{webp,gif}'
-                    ]
-                }]
-            },
-            server: {
-                files: [{
-                    expand: true,
-                    dot: true,
-                    cwd: '<%= appConfig.app %>/bower_components/font-awesome/fonts/',
-                    dest: '<%= appConfig.app %>/fonts/font-awesome',
-                    src: ['*']
-                }, {
-                    expand: true,
-                    dot: true,
-                    cwd: '<%= appConfig.app %>/bower_components/bootstrap/dist/fonts/',
-                    dest: '<%= appConfig.app %>/fonts/glyphicons',
-                    src: ['*']
-                }]
-            }
-        },
-    });
+        }
+    );
+
+    grunt.registerTask('build', [
+        'clean:dist',
+        'copy:server',
+        'html_minify',
+        'cssmin',
+        'copy'
+    ]);
+
+    grunt.registerTask('default', [
+        'clean',
+        'test',
+        'build'
+    ]);
 
     grunt.registerTask('serve', function (target) {
         if (target === 'dist') {
@@ -157,9 +216,9 @@ module.exports = function (grunt) {
         ]);
     });
 
-    grunt.registerTask('default', [
-        'clean',
-        'test',
-        'build'
+    grunt.registerTask('deploy', [
+        'build',
+        'sftp-deploy'
     ]);
-};
+}
+;
